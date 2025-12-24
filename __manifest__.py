35dc3a837b72370b50416f32f9ee35da97cd9d5b
Antonio Queb <cardstillo@gmail.com>
2025-12-24 15:30:52 -0600

    Actualizar __manifest__.py y vistas: agregar dependencia 'mail' y ajustar el orden de carga de los archivos; cambiar 'tree' a 'list' en las vistas para Odoo 19.

--- __manifest__.py
+++ __manifest__.py
@@ -4,12 +4,12 @@
     'author': 'Expert Developer',
     'category': 'Operations/Logistics',
     'summary': 'Control histórico de tarifas, KPIs y catálogo de fletes marítimos',
-    'depends': ['base', 'web', 'board'],
+    'depends': ['base', 'web', 'board', 'mail'], # He añadido 'mail' porque usas _inherit mail.thread
     'data': [
         'security/ir.model.access.csv',
-        'views/tarifario_menus.xml',
-        'views/tarifario_views.xml',
-        'views/dashboard_kpi.xml',
+        'views/tarifario_views.xml',     # 1. CARGA PRIMERO (Define la acción)
+        'views/tarifario_menus.xml',     # 2. CARGA DESPUÉS (Usa la acción)
+        'views/dashboard_kpi.xml',       # 3. CARGA AL FINAL (Usa los menús raíz)
     ],
     'assets': {
         'web.assets_backend': [
